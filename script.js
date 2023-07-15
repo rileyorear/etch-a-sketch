214b2a92ea9e13a386bffe46eb251de4abcf2709
rileyorear <113939466+rileyorear@users.noreply.github.com>
2023-07-14 19:28:42 -0700

    Add click and hold draw feature to grid

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const container = document.querySelector('.container');
+const body = document.querySelector('body');
 
-let size = 24;
+let size = 64;
 function updateBoxSize (size) {
   width = 500/size;
   return width;
 }
+
+let mouseDown = false;
+body.addEventListener('mousedown', () => {
+  mouseDown = true;
+  console.log(mouseDown);
+});
+body.addEventListener('mouseup', () => {
+  mouseDown = false;
+  console.log(mouseDown);
+});
 
 for (i = 0; i < size * size; i++) {
   const box = document.createElement('div');
@@ -12,6 +23,14 @@ for (i = 0; i < size * size; i++) {
   box.style.width = `${updateBoxSize(size)}px`;
   box.style.height = `${updateBoxSize(size)}px`;
   container.appendChild(box);
+  
+  box.addEventListener('mousedown', () => {
+    box.style.backgroundColor = 'black';
+  })
+  box.addEventListener('mouseover', () => {
+    if (mouseDown === true) {
+      box.style.backgroundColor = 'black';
+    }
+  });
 }
 
-  
