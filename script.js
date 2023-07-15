872b6c7beae601cd76656ef428ed870165e386c2
rileyorear <113939466+rileyorear@users.noreply.github.com>
2023-07-15 12:55:49 -0700

    Reorganize code and add change size feature

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector('.container');
 const body = document.querySelector('body');
+const sizeButton = document.querySelector('.sizeButton')
+
+let mouseDown = false;
+body.addEventListener('mousedown', () => {
+  mouseDown = true;
+});
+body.addEventListener('mouseup', () => {
+  mouseDown = false;
+});
 
 let size = 64;
 function updateBoxSize (size) {
@@ -7,30 +16,37 @@ function updateBoxSize (size) {
   return width;
 }
 
-let mouseDown = false;
-body.addEventListener('mousedown', () => {
-  mouseDown = true;
-  console.log(mouseDown);
-});
-body.addEventListener('mouseup', () => {
-  mouseDown = false;
-  console.log(mouseDown);
-});
-
-for (i = 0; i < size * size; i++) {
-  const box = document.createElement('div');
-  box.classList.add('grid-box')
-  box.style.width = `${updateBoxSize(size)}px`;
-  box.style.height = `${updateBoxSize(size)}px`;
-  container.appendChild(box);
-  
-  box.addEventListener('mousedown', () => {
-    box.style.backgroundColor = 'black';
-  })
-  box.addEventListener('mouseover', () => {
-    if (mouseDown === true) {
+function createGrid () {
+  for (i = 0; i < size * size; i++) {
+    const box = document.createElement('div');
+    box.classList.add('gridBox')
+    box.style.width = `${updateBoxSize(size)}px`;
+    box.style.height = `${updateBoxSize(size)}px`;
+    box.style.backgroundColor = 'white';
+    container.appendChild(box);
+    
+    box.addEventListener('mousedown', () => {
       box.style.backgroundColor = 'black';
-    }
-  });
+    })
+    box.addEventListener('mouseover', () => {
+      if (mouseDown === true) { box.style.backgroundColor = 'black'; }
+    });
+  }
 }
+createGrid();
+
+
+
+function removeGrid () {
+  const boxes = document.querySelectorAll('.gridBox');
+  for (box of boxes) {
+    box.remove();
+  }
+}
+
+sizeButton.addEventListener('mouseup', () => {
+  size = +prompt('Enter a number to set the width and height of the grid:', );
+  removeGrid();
+  createGrid();  
+});
 
